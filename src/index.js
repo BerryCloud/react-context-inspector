a68f9dd5c89c314f6675f6e8f39d56c9746888ad
Pavel Žák <pavel@zak.global>
2020-02-20 14:57:23 +0100

    Restructure into smaller functions

--- src/index.js
+++ src/index.js
@@ -1,77 +1,75 @@
 import React, { useContext } from 'react'
 
-export function createContextInspector (ContextType) {
-  if (!ContextType) {
-    throw new Error('Context type must be provided!')
-  }
-
-  let children = null
-  const eventHandlers = {
+function initEventHandlers () {
+  return {
     onFirstRender: [],
     onMount: [],
     onRender: [],
     waitFor: []
   }
+}
 
-  const status = {
+function initStatus () {
+  return {
+    children: null,
     mounted: false,
     rendered: false
   }
+}
 
-  /* The inspector must be a class component to provide
-   * compatibility with enzyme that currently cannot read
-   * props from functional components */
-  class TestInspector extends React.Component {
-    constructor (props) {
-      super(props)
-      this.invokeCallback = this.invokeCallback.bind(this)
-    }
-
+/* The inspector must be a class component to provide
+ * compatibility with enzyme that currently cannot read
+ * props from functional components */
+function createInspector (status, eventHandlers) {
+  return class TestInspector extends React.Component {
     componentDidMount () {
       status.mounted = true
       this.triggerEvent('onMount')
     }
 
-    invokeCallback (callback) {
-      return callback.call(this, this.props)
-    }
-
-    triggerEvent (eventName) {
-      return eventHandlers[eventName].every(this.invokeCallback)
-    }
+    invokeCallback = callback => callback.call(this, this.props)
+    triggerEvent = eventName => eventHandlers[eventName].every(this.invokeCallback)
 
     render () {
       if (!this.triggerEvent('waitFor')) {
         return null
       }
-      if (!status.rendered) {
-        this.triggerEvent('onFirstRender')
-      }
+      !status.rendered && this.triggerEvent('onFirstRender')
       this.triggerEvent('onRender')
       status.rendered = true
-      return children
+      return status.children
     }
   }
+}
+
+function provideCallback (TestComponent, eventHandlers, name) {
+  return function saveCallback (callback) {
+    eventHandlers[name].push(callback)
+    return TestComponent
+  }
+}
+
+export function createContextInspector (ContextType) {
+  if (!ContextType) {
+    throw new Error('Context type must be provided!')
+  }
+
+  const status = initStatus()
+  const eventHandlers = initEventHandlers()
+  const TestInspector = createInspector(status, eventHandlers)
 
   function TestComponent () {
     return <TestInspector {...useContext(ContextType)} />
   }
 
-  function provideCallback (name) {
-    return function saveCallback (callback) {
-      eventHandlers[name].push(callback)
-      return TestComponent
-    }
-  }
-
   TestComponent.status = status
   TestComponent.Inspector = TestInspector
-  TestComponent.onFirstRender = provideCallback('onFirstRender')
-  TestComponent.onMount = provideCallback('onMount')
-  TestComponent.onRender = provideCallback('onRender')
-  TestComponent.waitFor = provideCallback('waitFor')
+  TestComponent.onFirstRender = provideCallback(TestComponent, eventHandlers, 'onFirstRender')
+  TestComponent.onMount = provideCallback(TestComponent, eventHandlers, 'onMount')
+  TestComponent.onRender = provideCallback(TestComponent, eventHandlers, 'onRender')
+  TestComponent.waitFor = provideCallback(TestComponent, eventHandlers, 'waitFor')
   TestComponent.setChildren = function (useChildren) {
-    children = useChildren
+    status.children = useChildren
     return TestComponent
   }
   return TestComponent
